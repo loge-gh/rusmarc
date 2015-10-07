0d359d726adc4b1859bb5db8e880b16f218eaa0a
Viktor Ivakin <loge-gh@users.noreply.github.com>
2015-10-08 00:34:41 +0300

    fix

--- rusmarc/rusmarc_iter.py
+++ rusmarc/rusmarc_iter.py
@@ -18,13 +18,14 @@ class RusmarcFileIterator(object):
         return self
 
     def next(self):
-        rec_len_b = bytearray("00000")
-        if self.__f.readinto(rec_len_b):
+        rec_len_b = self.__f.read(5)
+        if rec_len_b:
             try:
                 rec_len = int(rec_len_b)
             except ValueError:
                 raise MalformedRecord
             rec = self.__f.read(rec_len - 5)
+            assert isinstance(rec, str)
             if rec[-1] == Rusmarc.IS1:
                 return Rusmarc(rec_len_b + rec, encoding=self.__enc)
             else:
